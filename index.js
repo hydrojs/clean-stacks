0a058c8b02653b15565e309d48fde545242d0ec5
Veselin Todorov <veselin@veselin.bg>
2013-12-22 19:06:23 +0200

    Handle errors without a stack

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ module.exports = function(hydro) {
 
   hydro.on('post:test', function(test) {
     if (test.status !== 'failed') return;
+    if (!test.error.stack) return;
     var stack = test.error.stack.split('\n').filter(function(line) {
       return line.indexOf('node_modules/hydro') === -1;
     });
